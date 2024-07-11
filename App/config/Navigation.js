f19ab4afd43f89ed7e8c0e851a3a1272412c8de5
Momen-Odeh <momen.odeh74@gmail.com>
2024-07-11 15:22:52 +0300

    made the modal Navigation Screen

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,9 +1,12 @@
+import { TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "../screens/Home";
 import Options from "../screens/Options";
 import CurrencyList from "../screens/CurrencyList";
+import { Entypo } from "@expo/vector-icons";
+import colors from "../constants/colors";
 
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
@@ -17,17 +20,46 @@ const MainStackScreen = () => (
       options={{ headerShown: false }}
     />
     <MainStack.Screen name="Options" component={Options} />
-    <MainStack.Screen
-      name="CurrencyList"
-      component={CurrencyList}
-      options={({ route }) => ({ title: route.params.title })}
-    />
   </MainStack.Navigator>
 );
+
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator
+    screenOptions={{
+      presentation: "modal",
+    }}
+  >
+    <ModalStack.Screen
+      name="Main"
+      component={MainStackScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList}
+      options={({ navigation, route }) => ({
+        title: route.params && route.params.title,
+        headerLeft: null,
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.pop()}
+            style={{ paddingHorizontal: 10 }}
+          >
+            <Entypo name="cross" size={30} color={colors.blue} />
+          </TouchableOpacity>
+        ),
+      })}
+    />
+  </ModalStack.Navigator>
+);
+
 const Navigation = () => (
   <NavigationContainer>
     <SafeAreaProvider>
-      <MainStackScreen />
+      <ModalStackScreen />
     </SafeAreaProvider>
   </NavigationContainer>
 );
