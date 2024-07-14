6bf4071cb9d596e798c83a7347d3f3b5f78622d7
Momen-Odeh <momen.odeh74@gmail.com>
2024-07-14 11:14:13 +0300

    make static API by using the promise to simulate the api behaviarl

--- App/index.js
+++ App/index.js
@@ -1,4 +1,9 @@
 import Navigation from "./config/Navigation";
+import { api } from "./util/api";
+
+api("/latest?base=USD")
+  .then((res) => console.log(res))
+  .catch((err) => console.log("err", err));
 
 export default function App() {
   return <Navigation />;
